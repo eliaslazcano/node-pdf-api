eb707e1df9e800749a2446bef450bca9352a0f3b
Elias Lazcano Castro Neto <elias304162@gmail.com>
2024-03-21 14:19:35 -0400

    Extraindo código para outro arquivo

--- app.js
+++ app.js
@@ -2,51 +2,15 @@ const express = require('express');
 const multer = require('multer');
 const axios = require('axios');
 const gs = require('ghostscript-node');
-const { PDFDocument, rgb } = require('pdf-lib');
+const { PDFDocument } = require('pdf-lib');
 const { setMaxListeners } = require('events');
+const { responderErro, tamanhoHumanizado, escreverPaginacao } = require('./helpers');
 
 const app = express();
 app.use(express.json()); //Habilita o interpretador de JSON do Express, caso contrario ele nao saberia o que é JSON.
 
 const porta = process.env.PORT || 3000;
 app.listen(porta, () => console.log(`SERVIDOR LIGADO. PORTA: ${porta}.`));
-
-const responderErro = (res, httpCode = 400, mensagem = '', errorId = 1, extra = {}) => {
-  return res.status(httpCode).json({http: httpCode, mensagem, erro: errorId, dados: extra});
-};
-
-const escreverPaginacao = (PDFPage, nrPagina) => {
-  if (typeof nrPagina !== 'number') nrPagina = parseInt(nrPagina);
-  PDFPage.drawRectangle({
-    x: (PDFPage.getWidth() - 22) ,
-    y: 3 ,
-    width: 50,
-    height: 12,
-    borderWidth: 1,
-    borderColor: rgb(0.7, 0.7, 0.7),
-    color: rgb(1, 1, 1),
-    opacity: 1,
-    borderOpacity: 1
-  });
-  PDFPage.drawText(nrPagina.toString(), {
-    x: (PDFPage.getWidth() - (nrPagina >= 100 ? 20 : (nrPagina >= 10 ? 18 : 14))),
-    y: 5,
-    size: 11,
-    color: rgb(0, 0, 0)
-  });
-};
-
-const tamanhoHumanizado = (bytes, binary = true) => {
-  const base = binary ? 1024 : 1000;
-  if (bytes < base) return `${bytes} B`;
-  const prefix = ['K', 'M', 'G'];
-  let unit = -1;
-  while (Math.abs(bytes) >= base && unit < prefix.length - 1) {
-    bytes /= base;
-    ++unit;
-  }
-  return `${bytes.toFixed(1)} ${prefix[unit]}B`;
-}
 
 const upload = multer();
 
